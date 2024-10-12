1b7316d75600fca567caaa73bef36956e957c4ff
Junaid Hassan <junaid.hassan@zeikh.com>
2024-10-12 05:12:15 +0500

    minor fixes

--- lib/data.ts
+++ lib/data.ts
@@ -2,9 +2,13 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import port3 from "@/public/port3.png";
+import port2 from "@/public/port2.png";
+import port1 from "@/public/port1.png";
+import port4 from "@/public/port4.png";
+import port5 from "@/public/port5.png";
+
+
 
 export const links = [
   {
@@ -34,53 +38,67 @@ export const links = [
 ] as const;
 
 export const experiencesData = [
-  {
-    title: "Virtual University",
-    location: "Lahore",
-    description:
-      "As I start studying it got internship.Throughout the internship, I worked on a variety of projects that allowed me to apply React.js concepts in real-world scenarios. These projects ranged from simple components to more complex interactive web applications.",
-    icon: React.createElement(LuGraduationCap),
-    date: "2021",
-  },
+ 
   {
     title: "Front-End Developer",
-    location: "Techstep",
+    location: "Ciel Technologies",
     description:
       "One of the fundamental concepts I learned was the component-based architecture of React.js. This approach enabled me to break down user interfaces into reusable and manageable pieces, leading to cleaner and more maintainable code",
     icon: React.createElement(CgWorkAlt),
-    date: "2021 - 2022",
+    date: "January 2022 - March 2024",
   },
   {
     title: "Full-Stack Developer",
-    location: "Lahore , Pakistan",
+    location: "Master IT",
     description:
       "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
     icon: React.createElement(FaReact),
-    date: "2021 - present",
+    date: "January 2017 - February 2022",
+  }, {
+    title: "Virtual University",
+    location: "Lahore",
+    description:
+      "As I start studying it got internship.Throughout the internship, I worked on a variety of projects that allowed me to apply Full Stack concepts in real-world scenarios. These projects ranged from simple components to more complex interactive web applications.",
+    icon: React.createElement(LuGraduationCap),
+    date: "2021",
   },
 ] as const;
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "GlassHive",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
+      "In Glasshive, I developed a seamless, interactive frontend that balances aesthetics with performance. Built using React.js, the interface offers a smooth, engaging user experience that simplifies SaaS management, keeping it responsive, visually appealing, and easy to navigate for users at all levels.",
+    tags: ["React", "Next.js", "MongoDB", "Tailwind", ],
+    imageUrl: port5,
+  },
+  {
+    title: "Stellar Development",
+    description:
+      " For Stellar Development, I crafted a user-friendly and responsive frontend in React.js, focusing on delivering a clean and intuitive design that enhances user interaction. This project seamlessly combines functionality with a polished layout, ensuring users can navigate features effortlessly while enjoying a visually cohesive experience.",
     tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+    imageUrl: port4,
   },
   {
-    title: "rmtDev",
+    title: "Podium",
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
+      " In building Podium, I developed a sleek, high-performance React.js interface that prioritizes both speed and user engagement. This design balances modern aesthetics with smooth navigation, ensuring users can access features seamlessly while enjoying an interactive and visually appealing experience.",
+    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
+    imageUrl: port3,
   },
   {
-    title: "Word Analytics",
+    title: "NearPeer",
     description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+      "For Nearpeer, I crafted a responsive, React-based frontend focused on fluid user interactions and optimized loading. Designed with a user-centric approach, this interface offers smooth transitions, intuitive navigation, and a visually engaging layout, making it easy for users to connect and interact with the platform’s features effortlessly.",
+    tags: ["React", "Node.js", "MongoDb", "Tailwind", "Redux"],
+    imageUrl: port2,
+  },
+  {
+    title: "Pulsetic",
+    description:
+      "For Pulsetic, I developed a sleek and responsive React.js frontend that brings real-time monitoring data to life.",
     tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+    imageUrl: port1,
   },
 ] as const;
 
